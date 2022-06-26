b595bccee91d6edf872fee99473b3348b0034bd8
JoelKanyi <joelkanyi98@gmail.com>
2022-06-26 11:55:33 +0300

    commit owner and user model classes

---
+++ app/src/main/java/com/kanyideveloper/dlight/domain/model/Owner.kt
@@ -0,0 +1,22 @@
+package com.kanyideveloper.dlight.domain.model
+
+data class Owner(
+    val avatarUrl: String,
+    val eventsUrl: String,
+    val followersUrl: String,
+    val followingUrl: String,
+    val gistsUrl: String,
+    val gravatarId: String,
+    val htmlUrl: String,
+    val id: Int,
+    val login: String,
+    val nodeId: String,
+    val organizationsUrl: String,
+    val receivedEventsUrl: String,
+    val reposUrl: String,
+    val siteAdmin: Boolean,
+    val starredUrl: String,
+    val subscriptionsUrl: String,
+    val type: String,
+    val url: String
+)
