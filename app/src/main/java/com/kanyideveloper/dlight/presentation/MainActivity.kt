b4963ff185359b9726d8f0fb743f0168c0530901
JoelKanyi <joelkanyi98@gmail.com>
2022-06-27 07:32:56 +0300

    added loading gifs and made some changes to theme

--- app/src/main/java/com/kanyideveloper/dlight/presentation/MainActivity.kt
+++ app/src/main/java/com/kanyideveloper/dlight/presentation/MainActivity.kt
@@ -3,14 +3,7 @@ package com.kanyideveloper.dlight.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.kanyideveloper.dlight.presentation.screens.search.NavGraphs
 import com.kanyideveloper.dlight.presentation.ui.theme.DlightTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
@@ -22,29 +15,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DlightTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Scaffold {
-                        DestinationsNavHost(navGraph = NavGraphs.root)
-                    }
+                Scaffold {
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DlightTheme {
-        Greeting("Android")
     }
 }
